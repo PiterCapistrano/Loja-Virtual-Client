019049b87f4c11130bf51a7b70ba8c8c3c3f5f4a
Piter Capistrano <pitercapistrano@gmail.com>
2024-10-04 21:05:40 -0300

    Craiação das Telas de Login e Cadastro

--- app/src/main/java/com/pitercapistrano/applojavirtualclient/activities/FormCadastro/FormCadastro.kt
+++ app/src/main/java/com/pitercapistrano/applojavirtualclient/activities/FormCadastro/FormCadastro.kt
@@ -1,21 +1,33 @@
 package com.pitercapistrano.applojavirtualclient.activities.FormCadastro
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pitercapistrano.applojavirtualclient.R
+import com.pitercapistrano.applojavirtualclient.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormCadastroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_form_cadastro)
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        window.statusBarColor = Color.parseColor("#000000")
+        supportActionBar!!.hide()
+
+        binding.btCadastrar.setOnClickListener {
+
         }
     }
 }
--- app/src/main/java/com/pitercapistrano/applojavirtualclient/activities/FormLogin/FormLogin.kt
+++ app/src/main/java/com/pitercapistrano/applojavirtualclient/activities/FormLogin/FormLogin.kt
@@ -1,21 +1,37 @@
 package com.pitercapistrano.applojavirtualclient.activities.FormLogin
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pitercapistrano.applojavirtualclient.R
+import com.pitercapistrano.applojavirtualclient.activities.FormCadastro.FormCadastro
+import com.pitercapistrano.applojavirtualclient.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        window.statusBarColor = Color.parseColor("#000000")
+        supportActionBar!!.hide()
+
+        binding.txtCadastrar.setOnClickListener {
+            val intent = Intent(this, FormCadastro::class.java)
+            startActivity(intent)
         }
     }
 }
