f6db060c0cba3ea8ab8cf514ee2b1395e237b302
Piter Capistrano <pitercapistrano@gmail.com>
2024-10-08 10:36:19 -0300

    Criação de Layout da tela de Detalhes de Produtos

--- app/src/main/java/com/pitercapistrano/applojavirtualclient/adapter/AdapterProduto.kt
+++ app/src/main/java/com/pitercapistrano/applojavirtualclient/adapter/AdapterProduto.kt
@@ -1,11 +1,13 @@
 package com.pitercapistrano.applojavirtualclient.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.pitercapistrano.applojavirtualclient.activities.DetalhesProduto.DetalhesProduto
 import com.pitercapistrano.applojavirtualclient.databinding.ProdutoItemBinding
 import com.pitercapistrano.applojavirtualclient.model.Produto
 
@@ -25,6 +27,11 @@ class AdapterProduto(val context: Context, val lista_produtos: MutableList<Produ
         Glide.with(context).load(lista_produtos.get(position).foto).into(holder.fotoProduto)
         holder.nomeProduto.text = lista_produtos.get(position).nome
         holder.precoProduto.text = "R$ ${lista_produtos.get(position).preco}"
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, DetalhesProduto::class.java)
+            context.startActivity(intent)
+        }
     }
 
     inner class ProdutoViewHolder(binding: ProdutoItemBinding) : RecyclerView.ViewHolder(binding.root){
