01f5caecf364a641f53b479b1c3f4ca3b033d504
Piter Capistrano <pitercapistrano@gmail.com>
2024-10-09 08:46:18 -0300

    Intergração com o Mercado Pago

--- app/src/main/java/com/pitercapistrano/applojavirtualclient/activities/Pagamento/Pagamento.java
+++ app/src/main/java/com/pitercapistrano/applojavirtualclient/activities/Pagamento/Pagamento.java
@@ -1,7 +1,9 @@
 package com.pitercapistrano.applojavirtualclient.activities.Pagamento;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.pitercapistrano.applojavirtualclient.Api.Api;
 import com.pitercapistrano.applojavirtualclient.R;
 import com.pitercapistrano.applojavirtualclient.databinding.ActivityPagamentoBinding;
@@ -24,6 +27,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class Pagamento extends AppCompatActivity {
 
     ActivityPagamentoBinding binding;
+    private String tamanho_calcado;
+    private String nome;
+    private String preco;
+
+    private final String PUBLIC_KEY ="APP_USR-df25dac5-4329-4449-80dd-2731d8ad3355";
+    private final String ACCESS_TOKEN ="APP_USR-5384308295965344-100822-cbb3cd209d4dd2c7dbb8f9894b5cc3f8-36119358";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +46,10 @@ public class Pagamento extends AppCompatActivity {
             return insets;
         });
 
-        String tamanho_calcado = getIntent().getExtras().getString("tamanho_calcado");
+        tamanho_calcado = getIntent().getExtras().getString("tamanho_calcado");
+        nome = getIntent().getExtras().getString("nome");
+        preco = getIntent().getExtras().getString("preco");
+
         Log.d("tamanho_calcado", tamanho_calcado);
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -51,7 +63,10 @@ public class Pagamento extends AppCompatActivity {
             String cep = binding.cep.getText().toString();
 
             if (cep.isEmpty()) {
-                Toast.makeText(this, "Preencha o Cep!", Toast.LENGTH_SHORT).show();
+                Snackbar snackbar = Snackbar.make(v, "Preencha o Cep!", Snackbar.LENGTH_SHORT);
+                snackbar.setBackgroundTint(Color.RED);
+                snackbar.setTextColor(Color.WHITE);
+                snackbar.show();
             } else {
                 api.setEndereco(cep).enqueue(new Callback<Endereco>() {
                     @Override
@@ -74,6 +89,24 @@ public class Pagamento extends AppCompatActivity {
                     }
                 });
             }
+        });
+
+        binding.btPagamento.setOnClickListener(v -> {
+           String cep = binding.cep.getText().toString();
+           String rua = binding.rua.getText().toString();
+           String numero = binding.numero.getText().toString();
+           String bairro = binding.bairro.getText().toString();
+           String cidade = binding.cidade.getText().toString();
+           String estado = binding.estado.getText().toString();
+
+           if (cep.isEmpty() || rua.isEmpty() || numero.isEmpty() || bairro.isEmpty() || cidade.isEmpty() || estado.isEmpty()){
+               Snackbar snackbar = Snackbar.make(v, "Preencha os campos obrigatórios!", Snackbar.LENGTH_SHORT);
+               snackbar.setBackgroundTint(Color.RED);
+               snackbar.setTextColor(Color.WHITE);
+               snackbar.show();
+           } else {
+
+           }
         });
     }
 
